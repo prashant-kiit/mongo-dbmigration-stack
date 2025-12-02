34c26c993698af9d05b0e2c1d71900bc587e5fc3
prashant-konovo <prashant.singh@konovo.com>
2025-12-03 01:09:56 +0530

    clean

--- migration-pipeline/lib/migration-stack.js
+++ migration-pipeline/lib/migration-stack.js
@@ -6,7 +6,7 @@ class MigrationStack extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    const { account, configuration } = props;
+    const { env, configuration } = props;
 
     const vpc = ec2.Vpc.fromLookup(this, "ExistingVpc", {
       vpcId: configuration.vpcId,
@@ -26,7 +26,7 @@ class MigrationStack extends cdk.Stack {
       subnets: [subnet1, subnet2]
     };
 
-    const project = new codebuild.Project(this, configuration.dbClusterName, {
+    const project = new codebuild.Project(this, configuration.newDbClusterName, {
       vpc: vpc,
       subnetSelection: subnetSelection,
       securityGroups: [sg],
@@ -49,7 +49,7 @@ class MigrationStack extends cdk.Stack {
               "cd /tmp/docdb-backup",
               "ls -al",
               "cd ~",
-              `mongorestore --host ${configuration.newDbClusterName}-${account.accountId}.us-east-1.docdb-elastic.amazonaws.com --port 27017 --username ${configuration.newAdminUsername} --password ${configuration.newAdminPassword} --ssl --authenticationMechanism SCRAM-SHA-1 /tmp/docdb-backup`,
+              `mongorestore --host ${configuration.newDbClusterName}-${env.account}.us-east-1.docdb-elastic.amazonaws.com --port 27017 --username ${configuration.newAdminUsername} --password ${configuration.newAdminPassword} --ssl --authenticationMechanism SCRAM-SHA-1 /tmp/docdb-backup`,
             ],
           },
         },
