a8f390402eac1e50d5d277a1cb793fadfc893269
prashant-konovo <prashant.singh@konovo.com>
2025-12-03 01:10:45 +0530

    feat: added describe migrtaion stack to build trigger to prevent param copy paste

--- migration-pipeline/trigger/start-build.js
+++ migration-pipeline/trigger/start-build.js
@@ -1,13 +1,41 @@
-const { CodeBuildClient, StartBuildCommand } = require("@aws-sdk/client-codebuild");
+const {
+  CloudFormationClient,
+  DescribeStacksCommand,
+} = require("@aws-sdk/client-cloudformation");
+const {
+  CodeBuildClient,
+  StartBuildCommand,
+} = require("@aws-sdk/client-codebuild");
 
-console.log("Triggering the migration build...");
+async function main() {
+  const client = new CloudFormationClient({ region: "us-east-1" });
 
-// const client = new CodeBuildClient({ region: "us-east-1" });
+  const res = await client.send(
+    new DescribeStacksCommand({
+      StackName: "MigrationStack",
+    })
+  );
 
-// const command = new StartBuildCommand({
-//   projectName: "PVLDocumentdbMigrationQualQ-T4i5IAedXYlR",
-// });
+  const outputs = res.Stacks[0].Outputs;
 
-// client.send(command).then((response) => {
-//   console.log("Triggered Build:", response.build.id);
-// });
+  const projectName = outputs.find(
+    (o) => o.OutputKey === "CodeBuildProjectName"
+  ).OutputValue;
+
+  console.log("CodeBuild Project Name =", projectName);
+
+  const codebuildClient = new CodeBuildClient({ region: "us-east-1" });
+
+  const command = new StartBuildCommand({
+    projectName: projectName,
+  });
+
+  const response = await codebuildClient.send(command);
+
+  console.log("Triggered Build:", response.build.id);
+}
+
+main().catch((error) => {
+  console.error("Error triggering build:", error);
+  process.exit(1);
+});
