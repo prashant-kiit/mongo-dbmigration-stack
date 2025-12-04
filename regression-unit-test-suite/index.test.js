bb8d65a90903aaee0d2d6bf58e745267ff098bd6
prashant-konovo-wsl <prashant.singh@konovo.com>
2025-12-05 00:42:51 +0530

    test: running only the test 7 and 8

--- regression-unit-test-suite/index.test.js
+++ regression-unit-test-suite/index.test.js
@@ -1,59 +1,57 @@
 const axios = require("axios");
 
-// case based switching for different tests
-
 describe("DocumentDB Migration API Tests", () => {
   const BASE_URL = "http://localhost:9001/v1/documentdbmigration";
 
-  test("Test 1: should fetch all surveys", async () => {
-    const res = await axios.get(`${BASE_URL}/test1`);
-    const data = res.data;
-    console.log("Test 1 data:", JSON.stringify(data, null, 2));
-    expect(data.oldSurveyCount).toBe(data.newSurveyCount);
-  });
+  // test("Test 1: should fetch all surveys", async () => {
+  //   const res = await axios.get(`${BASE_URL}/test1`);
+  //   const data = res.data;
+  //   console.log("Test 1 data:", JSON.stringify(data, null, 2));
+  //   expect(data.oldSurveyCount).toBe(data.newSurveyCount);
+  // });
 
-  test("Test 2: should fetch responders by responderObjectId", async () => {
-    const responderObjectId = "5e3c808ad16833762c17c5bc";
-    const res = await axios.get(
-      `${BASE_URL}/test2?responderObjectId=${responderObjectId}`
-    );
-    const data = res.data;
-    console.log("Test 2 data:", JSON.stringify(data, null, 2));
-    expect(data.oldResponderCount).toBe(data.newResponderCount);
-  });
+  // test("Test 2: should fetch responders by responderObjectId", async () => {
+  //   const responderObjectId = "5e3c808ad16833762c17c5bc";
+  //   const res = await axios.get(
+  //     `${BASE_URL}/test2?responderObjectId=${responderObjectId}`
+  //   );
+  //   const data = res.data;
+  //   console.log("Test 2 data:", JSON.stringify(data, null, 2));
+  //   expect(data.oldResponderCount).toBe(data.newResponderCount);
+  // });
 
-  test("Test 3: should fetch survey opt-out information", async () => {
-    const res = await axios.get(`${BASE_URL}/test3`);
-    const data = res.data;
-    console.log("Test 3 data:", JSON.stringify(data, null, 2));
-    expect(data.oldSurveyOptOutCount).toBe(data.newSurveyOptOutCount);
-  });
+  // test("Test 3: should fetch survey opt-out information", async () => {
+  //   const res = await axios.get(`${BASE_URL}/test3`);
+  //   const data = res.data;
+  //   console.log("Test 3 data:", JSON.stringify(data, null, 2));
+  //   expect(data.oldSurveyOptOutCount).toBe(data.newSurveyOptOutCount);
+  // });
 
-  test("Test 4: should find a survey by surveyId", async () => {
-    const surveyId = 24717;
-    const res = await axios.get(`${BASE_URL}/test4?surveyId=${surveyId}`);
-    const data = res.data;
-    console.log("Test 4 data:", JSON.stringify(data, null, 2));
-    expect(data.oldSurvey).toEqual(data.newSurvey);
-  });
+  // test("Test 4: should find a survey by surveyId", async () => {
+  //   const surveyId = 24717;
+  //   const res = await axios.get(`${BASE_URL}/test4?surveyId=${surveyId}`);
+  //   const data = res.data;
+  //   console.log("Test 4 data:", JSON.stringify(data, null, 2));
+  //   expect(data.oldSurvey).toEqual(data.newSurvey);
+  // });
 
-  test("Test 5: should verify responder existence in collection", async () => {
-    const res = await axios.get(`${BASE_URL}/test5`);
-    const data = res.data;
-    console.log("Test 5 data:", JSON.stringify(data, null, 2));
-    expect(data.oldResponders).toEqual(data.newResponders);
-  });
+  // test("Test 5: should verify responder existence in collection", async () => {
+  //   const res = await axios.get(`${BASE_URL}/test5`);
+  //   const data = res.data;
+  //   console.log("Test 5 data:", JSON.stringify(data, null, 2));
+  //   expect(data.oldResponders).toEqual(data.newResponders);
+  // });
 
-  test("Test 6: should update a survey record using surveyId and sampling flag", async () => {
-    const surveyIdToUpdate = 24717;
-    const isSampling = true;
-    const res = await axios.get(
-      `${BASE_URL}/test6?surveyIdToUpdate=${surveyIdToUpdate}&isSampling=${isSampling}`
-    );
-    const data = res.data;
-    console.log("Test 6 data:", JSON.stringify(data, null, 2));
-    expect(data.oldUpdateResult).toEqual(data.newUpdateResult);
-  });
+  // test("Test 6: should update a survey record using surveyId and sampling flag", async () => {
+  //   const surveyIdToUpdate = 24717;
+  //   const isSampling = true;
+  //   const res = await axios.get(
+  //     `${BASE_URL}/test6?surveyIdToUpdate=${surveyIdToUpdate}&isSampling=${isSampling}`
+  //   );
+  //   const data = res.data;
+  //   console.log("Test 6 data:", JSON.stringify(data, null, 2));
+  //   expect(data.oldUpdateResult).toEqual(data.newUpdateResult);
+  // });
 
   test("Test 7: should update multiple responders for a given survey", async () => {
     const surveyId = 15011;
@@ -150,14 +148,14 @@ describe("DocumentDB Migration API Tests", () => {
     expect(res.status).toBe(200);
   }, 20000);
 
-  test("Test 9: should replace survey opt-out entry by surveyId and email", async () => {
-    const surveyId = 24721;
-    const email = "prashant.singh@konovo.com";
+  // test("Test 9: should replace survey opt-out entry by surveyId and email", async () => {
+  //   const surveyId = 24721;
+  //   const email = "prashant.singh@konovo.com";
 
-    const res = await axios.get(`${BASE_URL}/test9?surveyId=${surveyId}&email=${email}`); 
-    const data = res.data;
-    console.log("Test 9 data:", JSON.stringify(data, null, 2));
-    expect(data.oldReplace).toEqual(data.newReplace);
-    expect(data.oldItems).toEqual(data.newItems);
-  });
+  //   const res = await axios.get(`${BASE_URL}/test9?surveyId=${surveyId}&email=${email}`); 
+  //   const data = res.data;
+  //   console.log("Test 9 data:", JSON.stringify(data, null, 2));
+  //   expect(data.oldReplace).toEqual(data.newReplace);
+  //   expect(data.oldItems).toEqual(data.newItems);
+  // });
 });
